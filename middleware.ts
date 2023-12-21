22c338ff4f2a634c3054c7b953ca87dec25e9bac
TheCryptoChad <adham_elneser@hotmail.com>
2023-12-21 13:58:24 -0500

    trying to fix webhook

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-	publicRoutes: ['/', '/api/webhook', '/questions/:id', '/tags', '/tags/:id', '/profile/:id', '/community', '/jobs'],
-	ignoredRoutes: ['/api/webhook', '/api/chatgpt'],
+	publicRoutes: ['/', '/api/webhook', '/api/webhooks(.*)', '/questions/:id', '/tags', '/tags/:id', '/profile/:id', '/community', '/jobs'],
+	ignoredRoutes: ['/api/webhook', '/api/webhooks(.*)', '/api/chatgpt'],
 });
 
 export const config = {
